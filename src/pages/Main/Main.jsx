17254fe61e4d5fb12ed535f494ebaabbde3324c9
LobetzSerg <lobet1501@gmail.com>
2024-06-29 17:18:35 +1000

    30

--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -10,10 +10,9 @@ import { PiTelegramLogoThin } from "react-icons/pi";
 import { useRef, useState } from 'react';
 import AccordeonItem from '../../components/accordeonItem/AccordeonItem';
 import ContactForm from '../../components/contactForm/ContactForm';
-// import gif02 from './../../media/gifs/gif5tr.gif';
-import gif02 from './../../media/gifs/housesgif.gif';
+import gif02 from './../../media/gifs/housegifcrop.gif';
 import gif03 from './../../media/gifs/gftr2.gif';
-import imgH from './../../media/images/house.jpg';
+import imgH from './../../media/room1.jpg';
 import elen2 from './../../media/photo/elen2.jpg';
 import valery from './../../media/photo/Валерия Главная.jpeg';
 import eva from './../../media/photo/Асатрян Ева.jpeg';
@@ -66,9 +65,9 @@ const Main = (props) => {
                     <h1>[02] <FiMinus size={25} /> КОМАНДА</h1>
                     <div className="teamWrapper">
                         <MemCard image={elen2} animation={textAnimation} about={'Основатель компании'} name={'Елена Григорян'} custom={3} aboutText={'Наша главная цель - развитие и рост. У нас нет подчиненных, в нашей команде только партнерские отношения. Мы современная компания, создающая тренды и нацеленная на результат.'} />
-                        <MemCard image={valery} animation={textAnimation2} about={'Руководитель отдела торговых помещений'} name={'Валерия Главная'} custom={3} aboutText={'Мы дружная, сплоченная команда единомышленников, влюбленных в свое дело!'} />
+                        <MemCard image={nastya} animation={textAnimation2} about={'Руководитель отдела офиса'} name={'Анастасия Краморенко'} custom={5} aboutText={'Брокер - это очень интересная, многогранная профессия, которая при должном усердии непременно ведет к успеху и высокому уровню заработка. А наша команда готова обучить всем необходимым навыкам и помочь в развитии в данном деле.'} />
                         <MemCard image={eva} animation={textAnimation} about={'Руководитель отдела офиса'} name={'Ева Асатрян'} custom={5} aboutText={'Недвижимость - это комплексная и динамическая сфера. Не существует универсального рецепта успеха. Важно изучать, анализировать, быть готовым к изменениям. И мы Вам с этим поможем!'} />
-                        <MemCard image={nastya} animation={textAnimation2} about={'Руководитель отдела офиса'} name={'Анастасия Краморенко'} custom={5} aboutText={'Мы дружная, сплоченная команда единомышленников, влюбленных в свое дело!'} />
+                        <MemCard image={valery} animation={textAnimation2} about={'Руководитель отдела торговых помещений'} name={'Валерия Главная'} custom={3} aboutText={'Мы дружная, сплоченная команда единомышленников, влюбленных в свое дело! Наша цель - развитие и рост.'} />
                     </div>
                 </div>
 
@@ -77,12 +76,12 @@ const Main = (props) => {
                     <h1>[03] <FiMinus size={25} /> НАШИ УСЛУГИ</h1>
                     <div className="servicesWrapper">
                         <div className="servicesText">
-                            <div className='serviceType'><span>1</span><motion.p custom={1} variants={textAnimation3}>Консультация клиента для формирования точного запроса и параметров объекта недвижимости</motion.p></div>
-                            <div className='serviceType'><span>2</span><motion.p custom={2} variants={textAnimation3}>Сопровождение сделки</motion.p></div>
-                            <div className='serviceType'><span>3</span><motion.p custom={3} variants={textAnimation3}>Оформление документов</motion.p></div>
-                            <div className='serviceType'><span>4</span><motion.p custom={4} variants={textAnimation3}>Банковское взаимодействие</motion.p></div>
-                            <div className='serviceType'><span>5</span><motion.p custom={5} variants={textAnimation3}>Юридическая проверка</motion.p></div>
-                            <div className='serviceType'><span>6</span><motion.p custom={6} variants={textAnimation3}>Подбор проектов с учетом пожеланий и потребностей</motion.p></div>
+                            <div className='serviceType'><span>1</span><motion.p custom={1} variants={textAnimation3}>Переговоры с потенциальными покупателями или арендаторами недвижимости</motion.p></div>
+                            <div className='serviceType'><span>2</span><motion.p custom={2} variants={textAnimation3}>Проведение просмотров объектов недвижимости</motion.p></div>
+                            <div className='serviceType'><span>3</span><motion.p custom={3} variants={textAnimation3}>Ведение полного цикла сделки от подбора объекта до заключения договора</motion.p></div>
+                            <div className='serviceType'><span>4</span><motion.p custom={4} variants={textAnimation3}>Ведение документации по сделкам с недвижимостью</motion.p></div>
+                            {/* <div className='serviceType'><span>5</span><motion.p custom={5} variants={textAnimation3}>Юридическая проверка</motion.p></div>
+                            <div className='serviceType'><span>6</span><motion.p custom={6} variants={textAnimation3}>Подбор проектов с учетом пожеланий и потребностей</motion.p></div> */}
                         </div>
                         <img src={gif03} alt="" />
                     </div>
@@ -102,10 +101,9 @@ const Main = (props) => {
                 <div className="accordeon" ref={props.accordeonRef}>
                     <h1>[05] <FiMinus size={25} /> ВОПРОС-ОТВЕТ</h1>
                     <div className="accorderonWrapper">
-                        <AccordeonItem headText={'Почему стоит обратиться в агенство недвижимости INVESTMENT CORPORATION?'} descrText={'Наша компания занимается недвижимостью уже не первый год и за это время мы стали профессионалами своего дела. Мы предоставляем полный спектр услуг в сфере покупки и продажи недвижимости, а также комплекс сопутствующих сервисов: подбор недвижимости, полное ипотечное и юридическое сопровождение, готовые решения для дизайна и отделки.'} />
-                        <AccordeonItem headText={'Почему выгодно покупать новостройку через агентство?'} descrText={'Обращаясь в агенство, Вы получаете массу преимуществ. Вы платите ту же стоимость, что у застройщика при этом получая приятные бонусы от агентства. Риэлтор подберет вариант, полностью отвечающий Вашим пожеланиям. Мы сотрудничаем только с надёжными застройщиками, поэтому вы защищены от мошеннических схем и убытка. Агенство предоставляет самые выгодные условия покупки недвижимости, а также информацию о скидках и акциях. Будете уверены в юридической чистоте сделки и соблюдении всех пунктов договора.'} />
-                        <AccordeonItem headText={'Первичное или вторичное жилье?'} descrText={'Первичное жилье, как правило, стоит дешевле, чем вторичное. При этом, после приобретения жилья, требуется дождаться, когда застройщик закончит строительство, а также потребуется выполнить ремонт.Первичное жилье гораздо проще оформить, чем жилье на вторичном рынке, особенно если квартира приобретается в ипотеку.При этом, на вторичном рынке больше заманчивых и интересных вариантом в различных районах города. Причём у Вас будет уверенность, что поблизости имеются детские сады, школы, магазины, ходит общественный транспорт и вся остальная инфраструктура.'} />
-                        <AccordeonItem headText={'В чем заключается наша работа?'} descrText={'Мы будем сопровождать Вас на всем пути к приобретению недвижимости: от выявления запроса до подписания документов о покупке.'} />
+                        <AccordeonItem headText={'Почему стоит обратиться в агенство недвижимости INVESTMENT CORPORATION?'} descrText={'ООО "ИНВЕСТМЕНТ КОРПОРЕЙШН" предоставляет полный комплекс риэлторских услуг на рынке недвижимости. Наше агенство работает на результат, оказывая качественные услуги от подбора до заключения. Нашими клиентами являются не только средние, но и крупные известные российские компании.'} />
+                        <AccordeonItem headText={'Почему выгодно сотрудничать с агенством?'} descrText={'Мы сотрудничаем только надежными арендодателями и собственниками.'} />
+                        <AccordeonItem headText={'В чем заключается наша работа?'} descrText={'Мы будем сопровождать Вас на всем пути сделки: от выявления запроса до подписания документов.'} />
                     </div>
                 </div>
 
